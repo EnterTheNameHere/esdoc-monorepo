ae0583c901dc1f4bd4c42335828637a17797de69
EnterTheNameHere Bohemian <email@enterthenamehere.com>
2021-01-29 19:08:43 +0100

    test: fix cli path

--- packages/esdoc-npm7-monorepo/packages/esdoc-publish-html-plugin/test/src/init.js
+++ packages/esdoc-npm7-monorepo/packages/esdoc-publish-html-plugin/test/src/init.js
@@ -2,7 +2,7 @@ const path = require('path');
 const ESDocCLI = require('@enterthenamehere/esdoc/out/ESDocCLI.js').default;
 
 function cli() {
-  const cliPath = path.resolve('./node_modules/esdoc/out/ESDocCLI.js');
+  const cliPath = path.resolve('./node_modules/@enterthenamehere/esdoc/out/ESDocCLI.js');
   const argv = ['node', cliPath, '-c', './test/fixture/esdoc.json'];
   const cli = new ESDocCLI(argv);
   cli.exec();
