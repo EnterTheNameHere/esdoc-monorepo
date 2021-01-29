4eb35f7624ca0e9badfbdf25d01d6a4216cf8835
EnterTheNameHere Bohemian <email@enterthenamehere.com>
2021-01-29 20:07:08 +0100

    fix: error if option is undefined

--- packages/esdoc-npm7-monorepo/packages/esdoc-integrate-manual-plugin/src/Plugin.js
+++ packages/esdoc-npm7-monorepo/packages/esdoc-integrate-manual-plugin/src/Plugin.js
@@ -6,12 +6,6 @@ class Plugin {
   onHandleDocs(ev) {
     this._docs = ev.data.docs;
     this._option = ev.data.option;
-    
-    if( !this._option.files || this._option.files.length === 0 ) {
-        let indexFileName = this._option.index || 'readme.md';
-        console.warn(`@enterthenamehere/esdoc-manual-plugin:\nNo files in option.files - if you just want to add readme.md file as the main page, just add "index": "${indexFileName}" to your esdoc top config. You don't need manual plugin for this. Otherwise specify which files consist your manual into the option.files for manual plugin.`);
-        return;
-    }
 
     this._exec();
   }
@@ -26,7 +20,6 @@ class Plugin {
   _setDefault() {
     if (!this._option) return;
 
-    assert(this._option.files);
     if (!('coverage' in this._option)) this._option.coverage = true;
   }
 
@@ -35,6 +28,12 @@ class Plugin {
     const results = [];
 
     if (!this._option) return results;
+    
+    if( !this._option.files || this._option.files.length === 0 ) {
+        const indexFileName = this._option.index || 'readme.md';
+        console.warn(`@enterthenamehere/esdoc-manual-plugin:\nNo files in option.files - if you just want to add readme.md file as the main page, just add "index": "${indexFileName}" to your esdoc top config. You don't need manual plugin for this. Otherwise specify which files consist your manual into the option.files for manual plugin.`);
+        return results;
+    }
 
     if (manual.index) {
       results.push({
