a9ab036f2b68808cb5f5737b1608f35bd93fa786
EnterTheNameHere Bohemian <email@enterthenamehere.com>
2021-06-19 10:07:18 +0200

    build: Display number of failed tests on the end

--- runForAll_NpmTest.ps1
+++ runForAll_NpmTest.ps1
@@ -1,123 +1,161 @@
 # Run in order of package use
 
 $originalLocation = Get-Location
-
+$numberOfFailedTests = 0
+    
 Write-Host $originalLocation
 
 cd "$originalLocation\common\color-logger"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\common\ice-cap"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-core"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-exclude-source-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-external-nodejs-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-external-webapi-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-importpath-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-inject-gtm-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-inject-script-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-inject-style-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-jsx-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-publish-markdown-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-typescript-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-lint-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-unexported-identifier-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-undocumented-identifier-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-type-inference-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-integrate-test-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-integrate-manual-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-external-ecmascript-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-ecmascript-proposal-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-coverage-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-brand-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-accessor-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-publish-html-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-standard-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-react-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\packages\esdoc-flow-type-plugin"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation\esdoc"
 Write-Host "Testing $(Get-Location)"
 npm run test
+$numberOfFailedTests = $numberOfFailedTests + $LastExitCode
 
 cd "$originalLocation"
+
+Write-Host ""
+Write-Host "======================================================================"
+Write-Host "    "
+Write-Host "    Number of failed tests: $numberOfFailedTests"
+Write-Host "    "
+Write-Host "======================================================================"
+Write-Host ""
