5e10b079b55f1969886070f93f5e3e46a78ff3dd
EnterTheNameHere Bohemian <email@enterthenamehere.com>
2021-08-22 10:51:54 +0200

    fix: source code line numbering is outside of box

--- packages/esdoc-publish-html-plugin/test/src/FileTest/File.test.js
+++ packages/esdoc-publish-html-plugin/test/src/FileTest/File.test.js
@@ -6,6 +6,6 @@ describe('test source code file', function () {
 
   it('has source code.', function () {
     assert.includes(doc, 'body [data-ice="title"]', 'src/Desc/Class.js');
-    assert.includes(doc, 'code[data-ice="content"]', 'export default class TestDescClass {');
+    assert.includes(doc, 'ol[data-ice="content"]', 'export default class TestDescClass {');
   });
 });
