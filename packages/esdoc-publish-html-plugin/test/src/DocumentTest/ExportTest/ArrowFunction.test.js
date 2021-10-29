30f2fdc23726f35503ae3981e3a960b10f59ad9d
EnterTheNameHere Bohemian <email@enterthenamehere.com>
2021-10-29 23:35:08 +0200

    test: updated tests for arrow function exports.

--- packages/esdoc-publish-html-plugin/test/src/DocumentTest/ExportTest/ArrowFunction.test.js
+++ packages/esdoc-publish-html-plugin/test/src/DocumentTest/ExportTest/ArrowFunction.test.js
@@ -1,39 +1,48 @@
-import {readDoc, assert, findParent} from './../../util.js';
+import { expect } from 'chai';
+import { loadCheerio } from './../../util.js';
 
 /** @test {FunctionDoc#@_name} */
 describe('test export arrow function', function () {
-  const doc = readDoc('function/index.html');
+  const $ = loadCheerio('function/index.html');
 
-  it('has default import path with direct arrow function definition.', function () {
-    findParent(doc, '[id="static-function-ArrowFunction"]', '[data-ice="detail"]', (doc)=>{
-      assert.includes(doc, '[data-ice="importPath"]', `import ArrowFunction from 'esdoc-test-fixture/src/Export/ArrowFunction.js'`);
+  describe('default anonymous arrow function export', function () {
+    const arrowFunctions = $('[id*="static-function-ArrowFunction"]').parents('[data-ice="detail"]');
+    expect( arrowFunctions.length ).to.equal(2);
+
+    it('has first anonymous arrow function', function () {
+      const firstArrowFunction = arrowFunctions.eq(0);
+      expect( $('[data-ice="access"]', firstArrowFunction).html() ).to.equal('public');
+      expect( $('[data-ice="name"]', firstArrowFunction).html() ).to.equal('ArrowFunction');
+      expect( $('[data-ice="importPath"]', firstArrowFunction).text() ).to.equal('import ArrowFunction from \'esdoc-test-fixture/src/Export/ArrowFunction.js\'');
+      expect( $('[data-ice="description"] p', firstArrowFunction).html() ).to.equal('this is ArrowFunction.');
     });
+
+    it('has second anonymous arrow function' ,function () {
+      const secondArrowFunction = arrowFunctions.eq(1);
+      expect( $('[data-ice="access"]', secondArrowFunction).html() ).to.equal('public');
+      expect( $('[data-ice="name"]', secondArrowFunction).html() ).to.equal('ArrowFunction');
+      expect( $('[data-ice="importPath"]', secondArrowFunction).text() ).to.equal('import ArrowFunction from \'esdoc-test-fixture/src/ArrowFunction.js\'');
+      expect( $('[data-ice="description"] p', secondArrowFunction).html() ).to.equal('This is another anonymous arrow function to test multiple anonymous default exports.');
+    })
   });
 
   it('has named import path with direct arrow function definition.', function () {
-    findParent(doc, '[id="static-function-testExportArrowFunction2"]', '[data-ice="detail"]', (doc)=>{
-      assert.includes(doc, '[data-ice="importPath"]', `import {testExportArrowFunction2} from 'esdoc-test-fixture/src/Export/ArrowFunction.js'`);
-    });
+    const importText = $('[id="static-function-testExportArrowFunction2"]').parents('[data-ice="detail"]').children('[data-ice="importPath"]').text();
+    expect( importText ).to.equal("import {testExportArrowFunction2} from 'esdoc-test-fixture/src/Export/ArrowFunction.js'");
   });
 
   it('is not documented with direct arrow function expression', function () {
-    try {
-      findParent(doc, '[id="static-function-testExportArrowFunction3"]', '[data-ice="detail"]', ()=>{});
-    } catch (e) {
-      return;
-    }
-    assert(false);
+    const thisFunctionIsNotExportedAndThusNotAvailable = $('[id="static-function-testExportArrowFunction3"]').parents('[data-ice="detail"]');
+    expect( thisFunctionIsNotExportedAndThusNotAvailable.length ).to.equal(0);
   });
 
   it('has named import path with undocument', function () {
-    findParent(doc, '[id="static-function-testExportArrowFunction4"]', '[data-ice="detail"]', (doc)=>{
-      assert.includes(doc, '[data-ice="importPath"]', `import {testExportArrowFunction4} from 'esdoc-test-fixture/src/Export/ArrowFunction.js'`);
-    });
+    const importText = $('[id="static-function-testExportArrowFunction4"]').parents('[data-ice="detail"]').children('[data-ice="importPath"]').text();
+    expect( importText ).to.equal("import {testExportArrowFunction4} from 'esdoc-test-fixture/src/Export/ArrowFunction.js'");
   });
 
   it('has named import path with indirect function definition.', function () {
-    findParent(doc, '[id="static-function-testExportArrowFunction5"]', '[data-ice="detail"]', (doc)=>{
-      assert.includes(doc, '[data-ice="importPath"]', `import {testExportArrowFunction5} from 'esdoc-test-fixture/src/Export/ArrowFunction.js'`);
-    });
+    const importText = $('[id="static-function-testExportArrowFunction5"]').parents('[data-ice="detail"]').children('[data-ice="importPath"]').text();
+    expect( importText ).to.equal("import {testExportArrowFunction5} from 'esdoc-test-fixture/src/Export/ArrowFunction.js'");
   });
 });
