e9da287bd82e5d0b0dcb95a489aa073453835a12
EnterTheNameHere Bohemian <email@enterthenamehere.com>
2022-05-13 15:46:57 +0200

    test: make find throw instead return undefined

--- packages/esdoc-core/test/util.js
+++ packages/esdoc-core/test/util.js
@@ -1,26 +1,36 @@
 import fs from 'fs-extra';
 
-export function find(key, ...values) {
-    const results = [];
+function find_imp(key, ...values) {
+  const results = [];
     
-    if(values.length === 1) {
-        return global.docs.find( (doc) => {
-            if(typeof values[0] === 'string') return doc[key] === values[0];
-            if(values[0] instanceof RegExp) return doc[key].match(values[0]);
-            return results;
-        });
-    }
+  if(values.length === 1) {
+    return global.docs.find( (doc) => {
+      if(typeof values[0] === 'string') return doc[key] === values[0];
+      if(values[0] instanceof RegExp) return doc[key].match(values[0]);
+      return results;
+    });
+  }
 
-    for(const value of values) {
-        const result = global.docs.find( (doc) => {
-            if (typeof value === 'string') return doc[key] === value;
-            if (value instanceof RegExp) return doc[key].match(value);
-            return '';
-        });
-        results.push(result);
-    }
+  for(const value of values) {
+    const result = global.docs.find( (doc) => {
+      if (typeof value === 'string') return doc[key] === value;
+      if (value instanceof RegExp) return doc[key].match(value);
+      return '';
+    });
+    results.push(result);
+  }
 
-    return results;
+  return results;
+}
+
+// We're overriding find to throw instead of return undefined for testing report
+export function find(key, ...values) {
+  const result = find_imp( key, ...values );
+  if( result === undefined ) {
+    throw new Error(`Key '${key}' with '${values}' value is not found in generated documents. If you are implementing new feature, check if source file was correctly parsed to AST and if *Doc correctly processed it to ESDoc format.`);
+  }
+
+  return result;
 }
 
 export function file(filePath) {
