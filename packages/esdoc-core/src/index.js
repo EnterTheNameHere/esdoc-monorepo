bfacc144f423b77e891d85057a3cce2649fd02db
EnterTheNameHere Bohemian <email@enterthenamehere.com>
2022-10-30 00:08:05 +0200

    refactor: adds index import file to centralize importing

---
+++ packages/esdoc-core/src/index.js
@@ -0,0 +1,50 @@
+
+import AssignmentDoc from './Doc/AssignmentDoc';
+import ClassDoc from './Doc/ClassDoc';
+import ClassPropertyDoc from './Doc/ClassPropertyDoc';
+import ExternalDoc from './Doc/ExternalDoc';
+import FileDoc from './Doc/FileDoc';
+import FunctionDoc from './Doc/FunctionDoc';
+import MemberDoc from './Doc/MemberDoc';
+import TypedefDoc from './Doc/TypedefDoc';
+import VariableDoc from './Doc/VariableDoc';
+
+import DocFactory from './Doc/AbstractDoc';
+
+import CommentParser from './Parser/CommentParser';
+import ESParser from './Parser/ESParser';
+import ParamParser from './Parser/ParamParser';
+
+import PluginManager from './Plugin/PluginManager';
+
+import ASTNodeContainer from './Util/ASTNodeContainer';
+import ASTUtil from './Util/ASTUtil';
+import { FileManager } from './Util/FileManager';
+import InvalidCodeLogger from './Util/InvalidCodeLogger';
+import NamingUtil from './Util/NamingUtil';
+import NPMUtil from './Util/NPMUtil';
+import PathResolver from './Util/PathResolver';
+
+export {
+  AssignmentDoc,
+  ClassDoc,
+  ClassPropertyDoc,
+  ExternalDoc,
+  FileDoc,
+  FunctionDoc,
+  MemberDoc,
+  TypedefDoc,
+  VariableDoc,
+  DocFactory,
+  CommentParser,
+  ESParser,
+  ParamParser,
+  PluginManager,
+  ASTNodeContainer,
+  ASTUtil,
+  FileManager,
+  InvalidCodeLogger,
+  NamingUtil,
+  NPMUtil,
+  PathResolver,
+};
