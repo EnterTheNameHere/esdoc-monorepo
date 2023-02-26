d4b86bd3311ad083ced6ef3c6cf98c7c4bd9fe5f
EnterTheNameHere Bohemian <email@enterthenamehere.com>
2023-02-26 08:12:00 +0100

    tests: updates test to include default value

--- packages/esdoc-standard-plugin/test/src/Plugin.test.js
+++ packages/esdoc-standard-plugin/test/src/Plugin.test.js
@@ -13,43 +13,123 @@ describe('test standard plugin:', function () {
         name: './src/Plugin.js',
         option: {
           brand: {
-            title: 'My Library'
+            author: null,
+            description: null,
+            image: null,
+            logo: null,
+            repository: null,
+            site: null,
+            title: 'My Library',
           },
           manual: {
-            files: ['./test/manual/overview.md']
+            asset: null,
+            files: ['./test/manual/overview.md'],
+            globalIndex: true,
+            index: 'readme.md',
           },
           test: {
-            source: "./test/test",
-            includes: ["Test.js$"],
-            excludes: ["\\.config\\.js$"],
-            interfaces: ["describe", "it", "context", "suite", "test"]
+            source: './test/test',
+            includes: ['Test.js$'],
+            excludes: ['\\.config\\.js$'],
+            interfaces: ['describe', 'it', 'context', 'suite', 'test']
           }
         }
       },
-      {name: '@enterthenamehere/esdoc-lint-plugin', option: {enable: true}},
-      {name: '@enterthenamehere/esdoc-coverage-plugin', option: {enable: true, kind: ['class', 'method', 'member', 'get', 'set', 'constructor', 'function', 'variable']}},
-      {name: '@enterthenamehere/esdoc-accessor-plugin', option: {access: ['public', 'protected', 'private'], autoPrivate: true}},
-      {name: '@enterthenamehere/esdoc-type-inference-plugin', option: {enable: true}},
-      {name: '@enterthenamehere/esdoc-ecmascript-proposal-plugin', option: {}},
-      {name: '@enterthenamehere/esdoc-external-ecmascript-plugin', option: {enable: true}},
-      {name: '@enterthenamehere/esdoc-brand-plugin', option: {title: 'My Library'}},
-      {name: '@enterthenamehere/esdoc-undocumented-identifier-plugin', option: {enable: true}},
-      {name: '@enterthenamehere/esdoc-unexported-identifier-plugin', option: {enable: false}},
-      {name: '@enterthenamehere/esdoc-integrate-manual-plugin', option: {
-        files: ['./test/manual/overview.md'],
-        //coverage: true
-      }},
-      {name: '@enterthenamehere/esdoc-integrate-test-plugin', option: {
-        source: "./test/test",
-        interfaces: ["describe", "it", "context", "suite", "test"],
-        includes: ["Test.js$"],
-        excludes: ["\\.config\\.js$"]
-      }},
-      {name: '@enterthenamehere/esdoc-publish-html-plugin', option: {}},
-      {name: './test/spy-plugin.js', option: {}}
+      {
+        name: '@enterthenamehere/esdoc-lint-plugin',
+        option: {
+          enable: true
+        }
+      },
+      {
+        name: '@enterthenamehere/esdoc-coverage-plugin',
+        option: {
+          enable: true,
+          kind: ['class', 'method', 'member', 'get', 'set', 'constructor', 'function', 'variable']
+        }
+      },
+      {
+        name: '@enterthenamehere/esdoc-accessor-plugin',
+        option: {
+          access: ['public', 'protected', 'private'],
+          autoPrivate: true
+        }
+      },
+      {
+        name: '@enterthenamehere/esdoc-type-inference-plugin',
+        option: {
+          enable: true
+        }
+      },
+      {
+        name: '@enterthenamehere/esdoc-ecmascript-proposal-plugin',
+        option: {
+          all: true,
+          asyncGenerators: true,
+          classProperties: true,
+          decorators: true,
+          doExpressions: true,
+          dynamicImport: true,
+          exportExtensions: true,
+          functionBind: true,
+          functionSent: true,
+          objectRestSpread: true,
+        }
+      },
+      {
+        name: '@enterthenamehere/esdoc-external-ecmascript-plugin',
+        option: {
+          enable: true
+        }
+      },
+      {
+        name: '@enterthenamehere/esdoc-brand-plugin',
+        option: {
+          author: null,
+          description: null,
+          image: null,
+          logo: null,
+          repository: null,
+          site: null,
+          title: 'My Library',
+        }
+      },
+      {
+        name: '@enterthenamehere/esdoc-undocumented-identifier-plugin',
+        option: {
+          enable: true
+        }
+      },
+      {
+        name: '@enterthenamehere/esdoc-integrate-manual-plugin',
+        option: {
+          asset: null,
+          files: ['./test/manual/overview.md'],
+          globalIndex: true,
+          index: 'readme.md',
+        }
+      },
+      {
+        name: '@enterthenamehere/esdoc-integrate-test-plugin',
+        option: {
+          source: './test/test',
+          includes: ['Test.js$'],
+          excludes: ['\\.config\\.js$'],
+          interfaces: ['describe', 'it', 'context', 'suite', 'test']
+        }
+      },
+      {
+        name: '@enterthenamehere/esdoc-publish-html-plugin',
+        option: {
+          template: null,
+        }
+      },
+      {
+        name: './test/spy-plugin.js',
+        option: {}
+      }
     ];
     
     assert.deepEqual(actual, expected);
-
   });
 });
