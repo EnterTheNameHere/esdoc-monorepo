054c41beed65f9bf81a58cda93750967bae82906
EnterTheNameHere Bohemian <email@enterthenamehere.com>
2023-07-27 09:00:48 +0200

    tests: fix wrong variable name

--- packages/esdoc-ecmascript-proposal-plugin/test/src/DynamicImport.js
+++ packages/esdoc-ecmascript-proposal-plugin/test/src/DynamicImport.js
@@ -1,7 +1,7 @@
 export default class DynamicImport {
   method() {
     for (let i = 0; i < 10; i++) {
-      import(`mod${$i}.js`)
+      import(`mod${i}.js`)
         .then(module => console.log(module))
         .catch(err => console.log(err));
     }
